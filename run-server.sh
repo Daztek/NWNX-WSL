0e7e2f82ccf45728aab1b3c5e14131a72bb53a52
Daz <31415508+Daztek@users.noreply.github.com>
2019-12-04 10:31:03 +0100

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -8,6 +8,7 @@ export NWNX_CORE_SKIP_ALL=y
 export NWNX_CORE_LOAD_PATH=~/unified/Binaries
 export NWNX_CORE_LOG_LEVEL=7
 
+export NWNX_SERVERLOGREDIRECTOR_SKIP=n
 export NWNX_SERVERLOGREDIRECTOR_LOG_LEVEL=6
 export NWNX_SQL_TYPE=SQLITE
 
@@ -20,7 +21,6 @@ export NWNX_CREATURE_SKIP=y
 export NWNX_DAMAGE_SKIP=y
 export NWNX_DATA_SKIP=y
 export NWNX_DIALOG_SKIP=y
-export NWNX_DOTNET_SKIP=y
 export NWNX_ELC_SKIP=y
 export NWNX_EFFECT_SKIP=y
 export NWNX_ENCOUNTER_SKIP=y
@@ -29,29 +29,32 @@ export NWNX_EXPERIMENTAL_SKIP=y
 export NWNX_FEEDBACK_SKIP=y
 export NWNX_ITEM_SKIP=y
 export NWNX_ITEMPROPERTY_SKIP=y
-export NWNX_LUA_SKIP=y
 export NWNX_MAXLEVEL_SKIP=y
-export NWNX_METRICS_INFLUXDB_SKIP=y
 export NWNX_OBJECT_SKIP=y
 export NWNX_PLAYER_SKIP=y
-export NWNX_PROFILER_SKIP=y
 export NWNX_RACE_SKIP=y
-export NWNX_REDIS_SKIP=y
 export NWNX_REGEX_SKIP=y
 export NWNX_REVEAL_SKIP=y
-export NWNX_RUBY_SKIP=y
 export NWNX_SQL_SKIP=y
-export NWNX_SERVERLOGREDIRECTOR_SKIP=n
 export NWNX_SKILLRANKS_SKIP=y
 export NWNX_SPELLCHECKER_SKIP=y
 export NWNX_THREADWATCHDOG_SKIP=y
 export NWNX_TIME_SKIP=y
-export NWNX_TRACKING_SKIP=y
 export NWNX_TWEAKS_SKIP=y
 export NWNX_UTIL_SKIP=y
 export NWNX_VISIBILITY_SKIP=y
 export NWNX_WEAPON_SKIP=y
 export NWNX_WEBHOOK_SKIP=y
+
+# These plugins are missing dependencies or outside configuration and won't work out of the box
+export NWNX_DOTNET_SKIP=y
+export NWNX_LUA_SKIP=y
+export NWNX_LUA_SKIP=y
+export NWNX_METRICS_INFLUXDB_SKIP=y
+export NWNX_PROFILER_SKIP=y
+export NWNX_REDIS_SKIP=y
+export NWNX_RUBY_SKIP=y
+export NWNX_TRACKING_SKIP=y
 
 LD_PRELOAD=~/unified/Binaries/NWNX_Core.so \
  ./nwserver-linux \
